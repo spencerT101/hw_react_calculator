abc2520c4e2e2be5c534a7aee0d328e8f55c94c8
Spencer <Spencer.Taber@yahoo.com>
2021-05-11 22:16:29 +0100

    UI test should update display passed

--- cypress/integration/App.spec.js
+++ cypress/integration/App.spec.js
@@ -7,4 +7,10 @@ describe("Calculator", () => {
     cy.get('#number2').click();
     cy.get('.display').should('contain', '2')
   })
+  it('should update display when number buttons are pressed', () => {
+    cy.get('#number3').click();
+    cy.get('#number7').click();
+    cy.get('#number2').click();
+    cy.get('.display').should('contain', '372')
+  })
 })
