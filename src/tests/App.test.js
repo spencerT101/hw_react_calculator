5486f921525a51be00688003b3c716851fac6247
Spencer <Spencer.Taber@yahoo.com>
2021-05-11 21:25:33 +0100

    divide test passed

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -28,7 +28,7 @@ describe('Calculator', () => {
     expect(runningTotal).toHaveTextContent('5')
   })
   it("should subtract 4 from 7 ", () => {
-    const button7 = container.getByTestId('number7'); 
+    const button7 = container.getByTestId('number7');
     const buttonSubtract = container.getByTestId('subtract');
     const button4 = container.getByTestId('number4');
     const buttonEquals = container.getByTestId('equals');
@@ -40,7 +40,7 @@ describe('Calculator', () => {
     expect(runningTotal).toHaveTextContent('3');
    })
 
-  it("should multiply 3 by 5 ", () => {
+  it("should multiply 3 by 5", () => {
      const button3 = container.getByTestId('number3');
      const buttonMultiply = container.getByTestId('multiply');
      const button5 = container.getByTestId('number5');
@@ -51,6 +51,22 @@ describe('Calculator', () => {
      fireEvent.click(button5)
      fireEvent.click(buttonEquals)
      expect(runningTotal).toHaveTextContent('15');
+   })
+
+  it("should divide 21 by 7 ", () => {
+    const button2 = container.getByTestId('number2');
+    const button1 = container.getByTestId('number1');
+    const buttonDivide = container.getByTestId('divide');
+    const button7 = container.getByTestId('number7');
+    const buttonEquals = container.getByTestId('equals');
+    const runningTotal = container.getByTestId('running-total');
+    fireEvent.click(button2)
+    fireEvent.click(button1)
+    fireEvent.click(buttonDivide)
+    fireEvent.click(button7)
+    fireEvent.click(buttonEquals)
+    expect(runningTotal).toHaveTextContent('3');
+
    })
 })
 
