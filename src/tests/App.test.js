0d9ab69dfed990511ba433929c06238259e47d47
Spencer <Spencer.Taber@yahoo.com>
2021-05-11 21:09:45 +0100

    passed unit test subtract

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -26,5 +26,17 @@ describe('Calculator', () => {
     fireEvent.click(buttonEquals)
     expect(runningTotal).toHaveTextContent('5')
   })
+  it("should subtract 4 from 7 ", () => {
+    const button7 = container.getByTestId('number7'); 
+    const buttonSubtract = container.getByTestId('subtract');
+    const button4 = container.getByTestId('number4');
+    const buttonEquals = container.getByTestId('equals');
+    const runningTotal = container.getByTestId('running-total');
+    fireEvent.click(button7)
+    fireEvent.click(buttonSubtract)
+    fireEvent.click(button4)
+    fireEvent.click(buttonEquals)
+    expect(runningTotal).toHaveTextContent('3')
+   });
 })
 
